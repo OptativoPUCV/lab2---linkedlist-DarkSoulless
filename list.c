e94a9ea19fa25875d1ad045deeed64beab8430f7
Replit user <>
2023-03-24 12:15:04 +0000

    new try

--- list.c
+++ list.c
@@ -72,7 +72,7 @@ void pushFront(List * list, void * data) {
   if(list->head) list->head->prev=nuevo;
   list->head=nuevo;
   nuevo->prev=NULL;
-  list->size++;
+  list->tail->size++;
 }
 
 void pushBack(List * list, void * data) {
